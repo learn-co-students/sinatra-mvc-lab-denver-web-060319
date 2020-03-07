8af47acfb7de0fd98a519212588c0144f68ca7cc
Fekade Senbeta <fekadesenbeta@gmail.com>
2020-03-07 15:40:35 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do
+        erb :user_input
+    end
+
+    post '/piglatinize' do
+        @pigObject = PigLatinizer.new
+        @user_phrase = params[:user_phrase]
+        @piglatinized = @pigObject.piglatinize(@user_phrase)
+
+        erb :piglatinizer
+    end
+
 end
